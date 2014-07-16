e0d89254cb2192859d75dce9041706e381e6c791
Amit Kohli <mexindian@gmail.com>
2014-07-17 00:52:54 +0200

    Applied better reactivity
    Fairly thorough re-write of server.R... put all the stuff that used to
    be in the reactive function into the renderplot function itself (it's
    reactive), Also figured out how to get actionbutton to stop everything.
    Few other things, and formatting improvements.

--- server.R
+++ server.R
@@ -9,42 +9,34 @@ the copyright laws for your country before posting these files!!
 
 
 shinyServer(function(input, output) {
-  
-  reut21578 <- system.file("texts", "crude", package = "tm")
-  c <- VCorpus(DirSource(reut21578), list(reader = readReut21578XMLasPlain))
-#   e <- reactive({input$data})
-  e <- reactive({input$data[1,4]})
-  
-  ## don't ask for that:
-  c <- tm_map(c, stripWhitespace)
-  c <- tm_map(c, removeNumbers)
-  #c <- tm_map(c, removePunctuation)
-  #c <- tm_map(c, removeWords, "xxxx")
-  #   c<-gsub("<.*?>","",c)
-  
-  # ask for this
-
-  change <- reactive({
-    if(input$update){
-      path <- input$data[1,4]
-      c <- Corpus(VectorSource(readLines(path)))
-      if(input$lower) c <- tm_map(c, content_transformer(tolower))
-      if(input$stem) c <- tm_map(c, stemDocument)
-      c <- tm_map(c, removeWords, stopwords(input$language))
+  observe({
+    # Run whenever button is pressed, then stay reactive.
+    if (input$update){
+      ## Start reactive statement
+      output$plot <- renderPlot({
+        ## Get data and clean a bit
+        path <- input$data[1,4]
+        c <- Corpus(VectorSource(readLines(path)))
+    
+        ## ask for this
+        if(input$lower) c <- tm_map(c, content_transformer(tolower))
+        if(input$stem) c <- tm_map(c, stemDocument)
+        
+        ## don't ask for that:
+        c <- tm_map(c, stripWhitespace)
+        c <- tm_map(c, removeNumbers)
+        c <- tm_map(c, removeWords, stopwords(input$language))
+        #c <- tm_map(c, removePunctuation)
+        #c <- tm_map(c, removeWords, "xxxx")
+        #       c<-gsub("<.*?>","",c)
+                
+        ## cloud itself
+        wordcloud(c, scale=c(3,0.5), max.words=input$max, 
+                                          use.r.layout=FALSE,
+                                          colors=brewer.pal(8,input$colorScheme),
+                                          random.order=FALSE)
+      
+      })
     }
-    })
-
-
-## cloud itself
-  output$plot <- renderPlot({
-    if(input$update){
-    wordcloud(change(), scale=c(3,0.5), max.words=input$max, 
-                                      use.r.layout=FALSE,
-                                      colors=brewer.pal(8,input$colorScheme),
-                                      random.order=FALSE)
-  }
   })
- output$class <- renderText({input$data[1,4]})
-output$class2 <- renderText("hello")
-
 })
